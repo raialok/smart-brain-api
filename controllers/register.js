0332620024500e3cbbda5208a3f3d896ee72d156
raialok <alok.rai@berkeley.edu>
2020-09-04 22:24:36 +0200

    input validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, bcrypt, db) => {
-  let { email, name, password } = req.body;
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect request");
+  }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
     trx
