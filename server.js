4bef8e4130266bc516fd55cb27cefe3c284a59e8
raialok <alok.rai@berkeley.edu>
2020-09-06 02:33:27 +0200

    adding db heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-metric-51492",
     user: "alokrai",
     password: "",
     database: "smart-brain",
